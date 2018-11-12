d0a2bd988a454ab1212f26d192d07713ba083ed5
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2018-11-12 21:54:11 +0200

    mini fix single_day

--- resources/views/single_day.blade.php
+++ resources/views/single_day.blade.php
@@ -2,40 +2,35 @@
     @section('content')
     
     <div class="container">
-                Pavadinimas
-                Kaina
-              <form class="form-group" action="{{route('store')}}" method="post">
-                {{csrf_field()}}
-                @foreach($soups as $soup)
-                <ul>
-                    <li>{{$soup->title}}</li>
-                    <li>{{$soup->price}} € <input name="soup" type="checkbox" value="{{$soup->title}}"></li>
-                </ul>
-                @endforeach 
-                @foreach($mains as $main)
-                <ul>
-                    <li>{{$main->title}}</li>
-                    <li>{{$main->price}} € <input name="main" type="checkbox" value="{{$main->title}}"></button></li>
-                </ul>
-                @endforeach 
-                @foreach($salads as $salad)
-                <ul>
-                    <li>{{$salad->title}}</li>
-                    <li>{{$salad->price}} € <input name="salad" type="checkbox" value="{{$salad->title}}"></li>
-                </ul>
-                @endforeach 
-                @foreach($sides as $side)
-                <ul>
-                    <li>{{$side->title}} ({{$side->type}})</li>
-                    <li><input name="side" type="checkbox" value="{{$side->title}}"></li>
-                </ul>
-                @endforeach
-                <ul>
-                    <li></li>
-                    <li>
-                        <button type="submit" class="btn btn-primary">Pirkti</button>
-                    </li>
-                </ul>
-            </form>
-</div>
-@endsection
+        Pavadinimas
+        Kaina
+        <form class="form-group" action="{{route('store')}}" method="post">
+            {{csrf_field()}}
+            @foreach($soups as $soup)
+            <ul>
+                <li>{{$soup->title}}</li>
+                <li>{{$soup->price}} € <input name="soup" type="checkbox" value="{{$soup->title}}"></li>
+            </ul>
+            @endforeach 
+            @foreach($mains as $main)
+            <ul>
+                <li>{{$main->title}}</li>
+                <li>{{$main->price}} € <input name="main" type="checkbox" value="{{$main->title}}"></button></li>
+            </ul>
+            @endforeach 
+            @foreach($salads as $salad)
+            <ul>
+                <li>{{$salad->title}}</li>
+                <li>{{$salad->price}} € <input name="salad" type="checkbox" value="{{$salad->title}}"></li>
+            </ul>
+            @endforeach 
+            @foreach($sides as $side)
+            <ul>
+                <li>{{$side->title}} ({{$side->type}})</li>
+                <li><input name="side" type="checkbox" value="{{$side->title}}"></li>
+            </ul>
+            @endforeach
+            <button type="submit" class="btn btn-primary">Pirkti</button>
+        </form>
+    </div>
+    @endsection
